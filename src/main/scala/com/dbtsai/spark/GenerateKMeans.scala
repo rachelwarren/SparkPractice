cbc3ff854009e4654014a5baf0e63aa3a250df75
RachelWarren <rachel@alpinenow.com>
2015-04-13 14:46:25 -0700

    A working version with random intialization and using util Vectors, eventually should support sparse vectors

--- src/main/scala/com/dbtsai/spark/GenerateKMeans.scala
+++ src/main/scala/com/dbtsai/spark/GenerateKMeans.scala
@@ -1,9 +1,13 @@
 package com.dbtsai.spark
 
 //import ...
+//import org.apache.spark.mllib.linalg._
+//import org.apache.spark.mllib.linalg.{SparseVector, DenseVector}
 
+import org.apache.spark.mllib.linalg.Vectors
 import org.apache.spark.rdd.RDD
 import org.apache.spark.{SparkConf, SparkContext}
+import org.apache.spark.util.Vector
 import math._
 import util._
 
@@ -15,24 +19,26 @@ import util._
 * k means, the repeated iterations will have to happen in the dirver
 * do i want the param to be the k or the array of clusters
  */
-class KMeans(centroids : Array[Array[Double]]) extends Serializable {
+class KMeans(centroids : Array[Vector]) extends Serializable {
   private val k : Int  = centroids.length
-  private val firstRow : Array[Double] = centroids(0)
+  private val firstRow : Vector = centroids(0)
   private val dim : Int = firstRow.length
-  private var c : Array[Array[Double]] = centroids
-  private var sums =  Array.fill(k,dim)(0.0)
-  private var counts = Array.fill(k, dim)(0)
+  private var c : Array[Vector] = centroids
+  private val zeroVector = Vector(Array.fill(dim)(0.0))
+  private var sums : Array[Vector] =  Array.fill(k)(zeroVector)
+  private var counts : Array[Vector] =  Array.fill(k)(zeroVector)
+
 
   def print = {
     var i = 0
     while(i < this.k){
-      var s = "centroid " + i + ": "
-      var j = 0
-      var temp = centroids(i)
-      while(j< this.dim){
-        s = s + temp(j) + ", "
-        j = j + 1
-      }
+      val s = "centroid " + i + ": " + centroids(i)
+//      var j = 0
+//      var temp = centroids(i)
+//      while(j< this.dim){
+//        s = s + temp(j) + ", "
+//        j = j + 1
+//      }
       println(s)
       i = i + 1
     }
@@ -47,17 +53,21 @@ class KMeans(centroids : Array[Array[Double]]) extends Serializable {
   * http://stackoverflow.com/questions/28949591/easiest-way-to-represent-euclidean-distance-in-scala
   *
    */
-  def distance(xs: Array[Double], ys: Array[Double]) = {
-    sqrt((xs zip ys).map { case (x,y) => pow(y - x, 2) }.sum)
-  }
 
-  def add(v: Array[Double]): this.type = {
+//  def sqdist(v1: Vector, v2: Vector) : Double = {
+//
+//
+//
+//  }
+
+  def add(v: Vector): this.type = {
     var bestIndex = 0
     var closest = Double.MaxValue
 
     var i = 0
     while( i < this.k) {
-      val tempD = distance(this.c(i), v)
+
+      val tempD = this.c(i).squaredDist(v)
       if (tempD < closest) {
         bestIndex = i
         closest = tempD
@@ -65,11 +75,13 @@ class KMeans(centroids : Array[Array[Double]]) extends Serializable {
       i +=1
     }
     i = 0
-    while (i < this.dim) {
-      this.sums(bestIndex)(i) = this.sums(bestIndex)(i)+ v(i)
-      this.counts(bestIndex)(i) = this.counts(bestIndex)(i) + 1
-      i +=1
-    }
+    this.sums(bestIndex) = this.sums(bestIndex).add(v)
+    this.counts(bestIndex) = this.counts(bestIndex).add(new Vector(Array.fill[Double](dim)(1.0)))
+//    while (i < this.dim) {
+//      this.sums(bestIndex)(i) = this.sums(bestIndex)(i)+ v(i)
+//      this.counts(bestIndex)(i) = this.counts(bestIndex)(i) + 1
+//      i +=1
+//    }
     this
   }
 
@@ -77,14 +89,19 @@ class KMeans(centroids : Array[Array[Double]]) extends Serializable {
     //replace this for loop with something less ugly
 
     var i = 0
-    while( i < this.k){
-      var j = 0
-      while( j < this.dim){
-        this.sums(i)(j) = this.sums(i)(j) + that.sums(i)(j)
-        this.counts(i)(j) = this.counts(i)(j) + that.counts(i)(j)
-        j +=1
-      }
-      i += 1
+//    while( i < this.k){
+//      var j = 0
+//      while( j < this.dim){
+//        this.sums(i)(j) = this.sums(i)(j) + that.sums(i)(j)
+//        this.counts(i)(j) = this.counts(i)(j) + that.counts(i)(j)
+//        j +=1
+//      }
+//      i += 1
+//    }
+    while (i < this.k){
+      this.sums(i) = this.sums(i).add(that.sums(i))
+      this.counts(i) = this.counts(i).add(that.counts(i))
+      i +=1
     }
     this
   }
@@ -93,43 +110,80 @@ class KMeans(centroids : Array[Array[Double]]) extends Serializable {
     var diff = 0.0
     var i  = 0
     while(i < this.k) {
-      val m = Array.fill[Double](dim)(0.0)
+      val m  = (Array.fill[Double](dim)(0.0))
       var j = 0
       val sTemp = this.sums(i)
       val cTemp = this.counts(i)
       while (j < this.dim) {
-        m(j) = if (cTemp(j) == 0) 0 else sTemp(j) / cTemp(j)
+        m(j) = if (cTemp(j) > 0.0)  sTemp(j) / cTemp(j) else 0.0
         j = j + 1
-      }
-      diff = diff + distance(m, centroids(i))
+    }
+      val center = Vector(m)
+      diff = diff + center.squaredDist(centroids(i))
       //update the centroids array
-      centroids(i) = m
+      centroids(i) = center
       i = i + 1
     }
     //zero out the sum and count arrays
-    this.sums =  Array.fill(k,dim)(0.0)
-    this.counts = Array.fill(k, dim)(0)
+    this.sums =  Array.fill(k)(new Vector(Array.fill(dim)(0.0)))
+    this.counts =  Array.fill(k)(new Vector(Array.fill(dim)(0.0)))
     diff
   }
-  def getCenters : Array[Array[Double]] = this.c
+  def getCenters : Array[Vector] = this.c
 }
 
-object GenerateKMeans {
+class KMeansModel {
+   var k : Int = 2
+   var convergence : Double = 0.01
+   var maxItterations : Int = 1000
+   var seed : Long = 42
+   var cols : Int = 5
   //uses the k-means ++ implementation given in the wikipedia article
   //http://en.wikipedia.org/wiki/K-means%2B%2B and implemented in ml lib
-  def KMeansRandom(input : RDD[String], k : Int, c : Double): Array[Array[Double]] = {
+  def setParams(kVal : Int, c : Double,  maxIt : Int, s : Long) = {
+    this.k = kVal
+    this.convergence = c
+    this.maxItterations = maxIt
+    this.seed = s
+  }
+  /*
+  utility method which given RDD input as string, converts it to a vector array of double s
+   */
+  def toDenseVector(input : RDD[String] )  = {
     val cachedRDD = input.cache()
-    val cols = cachedRDD.first().split(",").map(_.toDouble).length
-    //chose centers randomly
-    var centroids = cachedRDD.takeSample(false, k, 43).map(_.split(",").map(_.toDouble))
+    val vectors = cachedRDD.map(
+    line => new Vector(line.split(",").map(_.toDouble))
+    )
+    vectors
 
+  }
+//  def toSparseVector(cachedRDD : RDD[String]) = {
+//    val col = cachedRDD.first().split(",").map(_.toDouble).length
+//    val vectors = cachedRDD.map(
+//      line =>  {
+//        val a =  line.split(",").map(_.toDouble)
+//        new SparseVector(col, a.indices, a)
+//      }
+//    )
+//    vectors
+//  }
+
+  def getCentersRandom(input : RDD[Vector]): Array[Vector] = {
+    val cachedRDD = input.cache()
+    this.cols = cachedRDD.first().length
+    //chose centers randomly
+   cachedRDD.takeSample(false, k, seed)
+
+  }
+
+  def computeKMeans(cachedRDD : RDD[Vector], centroids : Array[Vector])= {
     var m = new KMeans(centroids)
     var distance = Double.MaxValue
     val max_itt = 100
     var itt = 0
-    while (distance > c && itt < max_itt) {
-      val next = cachedRDD.map(_.split(",").map(_.toDouble)).aggregate(m)(
-        (agg: KMeans, v: Array[Double]) => agg.add(v),
+    while (distance > convergence && itt < max_itt) {
+      val next = cachedRDD.aggregate(m)(
+        (agg: KMeans, v: Vector) => agg.add(v),
         (a: KMeans, b: KMeans) => a.merge(b)
       )
       distance = next.update
@@ -137,15 +191,24 @@ object GenerateKMeans {
       itt +=1
     }
     //logging information
-    if (distance > c) {
-      println("Warning, algorithm did not reach convergence threashold, " + c + ", in " + max_itt + "itterations. Distance = " + distance)
+    if (distance > convergence) {
+      println("Warning, algorithm did not reach convergence threashold, " + convergence + ", in " + max_itt + "itterations. Distance = " + distance)
     }
     else {
       println("K Means Random Completed, in " + itt + " iterations.")
     }
     //return the new centers
+    m.print
     m.getCenters
   }
 
 
 }
+// object KMeansModel{
+//   def apply(k, c, maxIt, s, input) = {
+//     val model = new KMeansModel
+//     model.setParams(k, c, maxIt, s)
+//     val centers = model.getCentersRandom(input)
+//     val means = model.computeKMeans(input, centers)
+//   }
+// }
